6bbaddb03253eff4a234a49b97d4bfd24f37a97d
codethread <adamhalldesigns@gmail.com>
2026-09-01 05:38:16 +0100

    chore: adopt deps-native workspace spools
    Squashed commits:
    - [8a5b251] chore: adopt deps-native workspace spools

--- .millstrand/init.clj
+++ .millstrand/init.clj
@@ -5,26 +5,21 @@
 
 ;; https://codethread.github.io/millstrand/docs/spools/customisation/
 (runtime/module! runtime :millstrand/spools-batteries
-                 {:ns 'millstrand.spools.batteries
-                  :spools ['millstrand.spools/batteries]})
+                 {:ns 'millstrand.spools.batteries})
 
 (runtime/module! runtime :module-me-help
                  {:file "me/help.clj"
-                  :spools ['millstrand.spools/batteries]
                   :after [:millstrand/spools-batteries]})
 
 (runtime/module! runtime :millhouse/spools-identity
                  {:ns 'millhouse.spools.identity
-                  :spools ['millhouse.spools/identity]
                   :required? true})
 
 (runtime/module! runtime :millhouse/spools-kanban
                  {:ns 'millhouse.spools.kanban
-                  :spools ['millhouse.spools/kanban]
                   :required? true})
 
 (runtime/module! runtime :module-me-config
                  {:file "me/config.clj"
-                  :spools ['ct.spools/harnesses 'millhouse.spools/identity]
                   :after [:millhouse/spools-identity]
                   :required? true})
